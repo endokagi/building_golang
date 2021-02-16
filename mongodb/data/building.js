381ae198cf59ce8d6291b2d807a9632d367d5fa1
Witthawat Tanoi <thewlip2008@gmail.com>
2021-02-16 14:02:13 +0700

    added mongodb docker testing

---
+++ mongodb/data/building.js
@@ -0,0 +1,26 @@
+db.createCollection("users")
+db.users.insert({
+    "firstname": "Witthawat",
+    "lastname": "Tanoi",
+    "email": "endozaki@wasd.com",
+    "password": "$2b$12$UREFwsRUoyF0CRqGNK0LzO0HM/jLhgUCNNIJ9RJAqMUQ74crlJ1Vu" 
+});
+db.users.insert({
+    "firstname": "Robert",
+    "lastname": "Baratheon",
+    "email": "mark_addy@gameofthron.es",
+    "password": "$2b$12$yGqxLG9LZpXA2xVDhuPnSOZd.VURVkz7wgOLY3pnO0s7u2S1ZO32y" 
+});
+db.users.insert({
+    "firstname": "Jaime",
+    "lastname": "Lannister",
+    "email": "nikolaj_coster-waldau@gameofthron.es",
+    "password": "$2b$12$6vz7wiwO.EI5Rilvq1zUc./9480gb1uPtXcahDxIadgyC3PS8XCUK" 
+});
+db.users.insert({
+    "firstname": "Catelyn",
+    "lastname": "Stark",
+    "email": "michelle_fairley@gameofthron.es",
+    "password": "$2b$12$fiaTH5Sh1zKNFX2i/FTEreWGjxoJxvmV7XL.qlfqCr8CwOxK.mZWS" 
+});
+
